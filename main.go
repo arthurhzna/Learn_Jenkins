700410ab0b3b5aa722f82ae1a58278e187db6116
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-09-29 16:11:48 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World! test Exclude branches that are also filed as PRs")
+	fmt.Println("Hello, World! test Exclude branches that are also filed a      s PRs")
 }
+
 
